ea8ffad1e36ec77a7ceab316d3e32bf05575a3e1
svitorsannttos <sebastiao.vitor@souunit.com.br>
2022-03-23 00:04:38 -0300

    Paginacao no endpoint estado

--- src/main/java/com/springboot/controller/EstadoController.java
+++ src/main/java/com/springboot/controller/EstadoController.java
@@ -3,10 +3,13 @@ package com.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.domain.Estado;
@@ -31,4 +34,15 @@ public class EstadoController {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<Estado>> listPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue="2") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue="ASC") String direction){
+		Page<Estado> obj = service.listPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(obj);
+		
+		//Valor 'direction' pode ser ASC ou DESC
+	}
 }
--- src/main/java/com/springboot/service/EstadoService.java
+++ src/main/java/com/springboot/service/EstadoService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.springboot.domain.Estado;
@@ -24,5 +27,8 @@ public class EstadoService {
 		return repo.findAll();
 	}
 	
-	
+	public Page<Estado> listPage(Integer page, Integer linesPerPage, String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
 }
